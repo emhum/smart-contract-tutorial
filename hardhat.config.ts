101332a0eb24f102f5056dd43327b04d3f604e35
emhum <565169676@qq.com>
2022-10-11 18:04:44 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,16 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-preprocessor';
+
+import fs from 'fs';
+
+function getRemappings() {
+  return fs
+    .readFileSync('remappings.txt', 'utf8')
+    .split('\n')
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split('='));
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,7 +22,22 @@ const config: HardhatUserConfig = {
       }
     }
   },
-  defaultNetwork: 'localhost'
+  defaultNetwork: 'localhost',
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      }
+    })
+  }
 };
 
 export default config;
